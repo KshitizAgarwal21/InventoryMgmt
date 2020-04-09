89707d8f54338428aeb46438fa31f4f3a3299e31
Abhishek Kushwaha <developer.abhishek1996@Gmail.com>
2020-04-09 14:25:33 +0530

    UI Modification Date 9/April/2020

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { NewSaleComponent } from './new-sale/new-sale.component';
+import { AddstockComponent } from './addstock/addstock.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [{path:'newSale', component:NewSaleComponent}];
+const routes: Routes = [
+
+  {path:'', component:HomeComponent},
+  {path:'newSale', component:NewSaleComponent},
+  {path:'addStock', component:AddstockComponent}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NewSaleComponent } from './new-sale/new-sale.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AddstockComponent } from './addstock/addstock.component';
+import { HomeComponent } from './home/home.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NewSaleComponent
+    NewSaleComponent,
+    NavbarComponent,
+    AddstockComponent,
+    HomeComponent,
+    SidenavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
